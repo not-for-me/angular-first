9e915691b8335dd15307be643dbc751030d04fcd
woojin.joe <woojin.joe@linecorp.com>
2017-03-26 20:47:28 +0900

    5장:mouse-pos-logger 기본 구현

---
+++ ch05/mouse-pos-logger/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { MouseTrackZoneComponent } from './mouse-track-zone/mouse-track-zone.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MouseTrackZoneComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
