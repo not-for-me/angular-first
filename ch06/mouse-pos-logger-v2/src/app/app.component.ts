52450e450d299fb53c3a49920746e49e664afb3e
woojin.joe <woojin.joe@linecorp.com>
2017-03-26 21:31:55 +0900

    6장:mouse-pos-logger-v2 웹 컴포넌트

---
+++ ch06/mouse-pos-logger-v2/src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { MySpecialLoggerService } from './my-special-logger.service';
+import { LogLevel } from './log-level.enum';
+
+@Component({
+  selector: 'mpl-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mpl works!';
+
+  constructor(private logger: MySpecialLoggerService) {
+    this.logger = new MySpecialLoggerService(LogLevel.INFO);
+    // this.testLoggerLevel();
+  }
+
+  printDebugLog() {
+    this.logger.debug("test depenency injector tree!");
+  }
+
+  testLoggerLevel() {
+    console.log("==========Default(Info) Log Level==========");
+    this.logger.debug("test logging... in debug ");
+    this.logger.info("test logging... in info ");
+    this.logger.warn("test logging... in warn ");
+    this.logger.error("test logging... in error ");
+
+    this.logger.logLevel = LogLevel.DEBUG;
+    console.log("==========Debug Log Level==========");
+    this.logger.debug("test logging... in debug ");
+    this.logger.info("test logging... in info ");
+    this.logger.warn("test logging... in warn ");
+    this.logger.error("test logging... in error ");
+
+    this.logger.logLevel = LogLevel.WARN;
+    console.log("==========WARN Log Level==========");
+    this.logger.debug("test logging... in debug ");
+    this.logger.info("test logging... in info ");
+    this.logger.warn("test logging... in warn ");
+    this.logger.error("test logging... in error ");
+
+    this.logger.logLevel = LogLevel.ERROR;
+    console.log("==========Error Log Level==========");
+    this.logger.debug("test logging... in debug ");
+    this.logger.info("test logging... in info ");
+    this.logger.warn("test logging... in warn ");
+    this.logger.error("test logging... in error ");
+  }
+}
