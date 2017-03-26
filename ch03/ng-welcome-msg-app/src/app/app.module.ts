3814bcd1b259c9bd5cc4d63ed123d55fa7af1ecd
woojin.joe <woojin.joe@linecorp.com>
2017-03-26 14:56:22 +0900

    3장:ng-weclome-msg-app 뷰를 구성하는 요소

--- ch03/ng-welcome-msg-app/src/app/app.module.ts
+++ ch03/ng-welcome-msg-app/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WelcomeMsgComponent } from './welcome-msg/welcome-msg.component';
+import { LangSelectorComponent } from './lang-selector/lang-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeMsgComponent,
+    LangSelectorComponent
   ],
   imports: [
     BrowserModule,
