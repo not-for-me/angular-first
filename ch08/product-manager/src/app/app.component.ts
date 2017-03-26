cd82cc28f7463ef413bf6a0ebffbf9de371599e5
woojin.joe <woojin.joe@linecorp.com>
2017-03-27 00:55:21 +0900

    8장:product-manager 기본 코드

---
+++ ch08/product-manager/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '상품 등록';
+  product: any;
+
+  constructor() {
+    this.initProduct();
+  }
+
+  initProduct() {
+    this.product = { name: '', listPrice: '', qty: 0, desc: '' };
+  }
+
+  onSubmit() {
+    alert(`제출\n${JSON.stringify(this.product)}`);
+  }
+
+  onReset() {
+    this.initProduct();
+  }
+
+}
