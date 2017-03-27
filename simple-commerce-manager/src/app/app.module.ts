c243ec0babe7996d7c3dca4d37b2e4cc43d989cd
woojin.joe <woojin.joe@linecorp.com>
2017-03-27 09:03:02 +0900

    SCM: 카테고리 도메인 구현

--- simple-commerce-manager/src/app/app.module.ts
+++ simple-commerce-manager/src/app/app.module.ts
@@ -2,8 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from 'angularfire2';
 import { firebaseConfig, firebaseAuthConfig } from '../firebase.config';
+import { ToastModule, ToastOptions } from 'ng2-toastr';
+import { CustomToastOptions } from './custom-toast-options'
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { ScmMainModule } from './scm-main/scm-main.module'
 import { ProductModule } from './product/product.module';
@@ -18,15 +22,17 @@ import { AppComponent } from './app.component';
   declarations: [AppComponent],
   imports: [
     /* Angular Modules*/
-    BrowserModule, FormsModule, HttpModule, 
+    BrowserModule, FormsModule, HttpModule, BrowserAnimationsModule,
 
     /* App Modules */
     ScmMainModule, ProductModule, CategoryModule,
     AppRoutingModule, SharedModule,
     /* 3rd Modules */
     AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig),
+    ToastModule.forRoot(),
+    NgbPaginationModule.forRoot(),
   ],
-  providers: [],
+  providers: [{ provide: ToastOptions, useClass: CustomToastOptions }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
